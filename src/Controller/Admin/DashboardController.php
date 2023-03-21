4bd62338a9607049cacfc2c0af5969e97ea6dfd4
enzdo <enzodeniau2003@gmail.com>
2023-03-21 23:16:55 +0100

    :sparkles: Super Admin + CRUD

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -5,10 +5,12 @@ namespace App\Controller\Admin;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Station;
+use App\Entity\Domain;
 
 class DashboardController extends AbstractDashboardController
 {
@@ -34,6 +36,11 @@ class DashboardController extends AbstractDashboardController
         // return $this->render('some/path/my-dashboard.html.twig');
     }
 
+    public function __construct(AuthorizationCheckerInterface $authChecker)
+    {
+        $this->authChecker = $authChecker;
+    }
+
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
@@ -42,6 +49,10 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToCrud('Station', 'fas fa-list', Station::class);
+        yield MenuItem::linkToCrud('Station', 'fa fa-snowflake-o', Station::class);
+        if ($this->authChecker->isGranted('ROLE_SU')) {
+            yield MenuItem::linkToCrud('Domain', ' fa fa-map-o', Domain::class)
+                ->setPermission('ROLE_SU');
+        }
     }
 }
