e6e27ea9af23f44c39d53f833fe72e1a1fae7ce9
TuberculeP <lavieville.felix@gmail.com>
2023-03-26 18:42:51 +0200

    Merging Station components by Thomas Filhol

--- src/Controller/StationController.php
+++ src/Controller/StationController.php
@@ -14,50 +14,58 @@ use App\Entity\Problem;
 use App\Repository\ProblemRepository;
 use App\Repository\LiftRepository;
 use App\Entity\Lift;
+use App\Repository\SlopeRepository;
+use App\Entity\Slope;
 
 
 class StationController extends AbstractController
 {
     #[Route('/station/{id}', name: 'app_station_show')]
-    public function show(Request $request, Station $station, EntityManagerInterface $entityManager, ProblemRepository $problems, LiftRepository $liftRepository): Response
-    {
-        $form = $this->createFormBuilder()
-            ->add('title', TextType::class)
-            ->add('description', TextareaType::class)
-            ->getForm();
+    public function show(Request $request, Station $station, EntityManagerInterface $entityManager, ProblemRepository $problems, LiftRepository $liftRepository, SlopeRepository $slopeRepository): Response
+{
+    $form = $this->createFormBuilder()
+        ->add('title', TextType::class)
+        ->add('description', TextareaType::class)
+        ->getForm();
 
-        $form->handleRequest($request);
+    $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $data = $form->getData();
+    if ($form->isSubmitted() && $form->isValid()) {
+        $data = $form->getData();
 
-            // Enregistrer les données dans la base de données
-            $problem = new Problem();
-            $problem->setStation($station);
-            $problem->setTitle($data['title']);
-            $problem->setDescription($data['description']);
-            $problem->setDate(new \DateTime('now'));
-
-
-            $entityManager->persist($problem);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('app_station_show', ['id' => $station->getId()]);
-        }
-
-        $problems = $problems->findBy(
-            ['station' => $station],  // Critères de recherche
-            ['date' => 'DESC'],       // Tri par date décroissante
-            3                        // Limite de 3 résultats
-        );
-        $lifts = $liftRepository->findBy(['station' => $station]);
-        return $this->render('station/show.html.twig', [
-            'station' => $station,
-            'form' => $form->createView(),
-            'problems' => $problems,
-            'lifts' => $lifts
-        ]);
+        // Enregistrer les données dans la base de données
+        $problem = new Problem();
+        $problem->setStation($station);
+        $problem->setTitle($data['title']);
+        $problem->setDescription($data['description']);
+        $problem->setDate(new \DateTime('now'));
 
 
+        $entityManager->persist($problem);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_station_show', ['id' => $station->getId()]);
     }
+
+    $problems = $problems->findBy(
+        ['station' => $station],  // Critères de recherche
+        ['date' => 'DESC'],       // Tri par date décroissante
+        3                        // Limite de 3 résultats
+    );
+    
+    $lifts = $liftRepository->findBy(['station' => $station]);
+    $slopes = $slopeRepository->findBy(['station' => $station]);    
+
+    return $this->render('station/show.html.twig', [
+        'station' => $station,
+        'form' => $form->createView(),
+        'problems' => $problems,
+        'lifts' => $lifts,
+        'slopes' => $slopes,
+    ]);
+
+    
 }
+}
+
+
