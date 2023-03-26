82f01ff9841e39136144876108f06d7b815bdd42
Thomas Filhol <tom.filhol@gmail.com>
2023-03-26 15:33:30 +0200

    lol

--- src/Command/WebsocketServerCommand.php
+++ src/Command/WebsocketServerCommand.php
@@ -12,7 +12,7 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class WebsocketServerCommand extends Command
 {
-    protected static $defaultName = "run:websocket";
+    protected static $defaultName = "run:websocket-server";
     protected MessageHandler $messageHandler;
 
     public function __construct(MessageHandler $messageHandler)
--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller\Admin;
 
-use App\Entity\Lift;
-use App\Entity\Slope;
+use App\Entity\Problem;
+use App\Entity\Station;
+use App\Repository\ProblemRepository;
+use App\Repository\LiftRepository;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -16,26 +18,18 @@ use App\Entity\Domain;
 
 class DashboardController extends AbstractDashboardController
 {
+    private $entityManager;
+
+    public function __construct(\Doctrine\ORM\EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
-
-
-        // Option 1. You can make your dashboard redirect to some common page of your backend
-        //
         $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
         return $this->redirect($adminUrlGenerator->setController(StationCrudController::class)->generateUrl());
-
-        // Option 2. You can make your dashboard redirect to different pages depending on the user
-        //
-        // if ('jane' === $this->getUser()->getUsername()) {
-        //     return $this->redirect('...');
-        // }
-
-        // Option 3. You can render some custom template to display a proper dashboard with widgets, etc.
-        // (tip: it's easier if your template extends from @EasyAdmin/page/content.html.twig)
-        //
-        // return $this->render('some/path/my-dashboard.html.twig');
     }
 
     public function __construct(AuthorizationCheckerInterface $authChecker)
@@ -51,15 +45,16 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
+        $problemRepository = $this->entityManager->getRepository(Problem::class);
+
         if ($this->authChecker->isGranted('ROLE_SU')) {
             yield MenuItem::linkToCrud('Domain', ' fa fa-map-o', Domain::class)
                 ->setPermission('ROLE_SU');
         }
-<<<<<<< HEAD
-        yield MenuItem::linkToCrud('Lift', 'fa fa-person-ski-lift', Lift::class);
-        yield MenuItem::linkToCrud('Slope', 'fa fa-solid fa-angle', Slope::class);
-=======
         yield MenuItem::linkToCrud('Station', 'fa fa-snowflake-o', Station::class);
->>>>>>> 2cd37fc1d29dc25175a5af3257ac1751e5b1c504
+
+        if ($problemRepository->count([]) > 0) {
+            yield MenuItem::linkToCrud('Problèmes', 'fas fa-list', Problem::class);
+        }
     }
 }
